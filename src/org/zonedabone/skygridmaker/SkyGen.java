9552131b122425fd9c07a3396fd1c50b1e4a23b7
zonedabone <zonedabone@gmail.com>
2012-05-03 17:31:47 -0400

    Prevent liquid updates on new blocks.

--- src/org/zonedabone/skygridmaker/SkyGen.java
+++ src/org/zonedabone/skygridmaker/SkyGen.java
@@ -3,6 +3,7 @@ package org.zonedabone.skygridmaker;
 import java.util.Random;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Chest;
 import org.bukkit.generator.ChunkGenerator;
@@ -11,6 +12,15 @@ import org.bukkit.inventory.ItemStack;
 
 public class SkyGen extends ChunkGenerator {
 	
+	@Override
+	public Location getFixedSpawnLocation(World world, Random random) {
+		return world.getBlockAt(0, 129, 0).getLocation();
+	}
+	
+	@Override
+	public boolean canSpawn(World world, int x, int z) {
+		return (x % 4 == 0 && z % 4 == 0);
+	}
 	byte[] blocks = new byte[]{1, 2, 3, 4, 7, 8, 10, 12, 13, 14, 15, 16, 17, 18, 21, 24, 35, 54};
 	byte[] naturals = new byte[]{6, 37, 38, 39, 40};
 	private SkyGridMaker plugin;
@@ -37,6 +47,8 @@ public class SkyGen extends ChunkGenerator {
 						}
 					} else if (b == 54) {
 						Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new ChestFiller(cx, cz, x, y, z, plugin, world));
+					} else if (b == 35) {
+						Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new WoolMaker(cx, cz, x, y, z, plugin, world));
 					}
 				}
 			}
@@ -77,6 +89,25 @@ public class SkyGen extends ChunkGenerator {
 			for (int i = 0; i < Math.random() * inv.getSize(); i++) {
 				inv.setItem((int) (Math.random() * inv.getSize()), new ItemStack((int) (Math.random() * 100), (int) (Math.random() * 10)));
 			}
+		}
+	}
+	private class WoolMaker implements Runnable {
+		
+		private int cx, cz, x, y, z;
+		private World w;
+		
+		public WoolMaker(int cx, int cz, int x, int y, int z, SkyGridMaker plugin, World w) {
+			this.cx = cx;
+			this.cz = cz;
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.w = w;
+		}
+		
+		@Override
+		public void run() {
+			w.getChunkAt(cx, cz).getBlock(x, y, z).setData((byte) (Math.random() * 15));
 		}
 	}
 }
--- src/org/zonedabone/skygridmaker/SkyGridMaker.java
+++ src/org/zonedabone/skygridmaker/SkyGridMaker.java
@@ -1,12 +1,41 @@
 package org.zonedabone.skygridmaker;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class SkyGridMaker extends JavaPlugin {
+public class SkyGridMaker extends JavaPlugin implements Listener {
+	
+	public final static BlockFace[] faces = new BlockFace[]{BlockFace.UP, BlockFace.DOWN, BlockFace.NORTH, BlockFace.SOUTH, BlockFace.EAST, BlockFace.WEST};
+	
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
 	
 	@Override
 	public ChunkGenerator getDefaultWorldGenerator(String worldName, String id) {
 		return new SkyGen(this);
+	}
+	
+	@EventHandler
+	public void onBlockFromTo(BlockFromToEvent e) {
+		if (e.getBlock().getWorld().getGenerator() != null && e.getBlock().getWorld().getGenerator().getClass().getName().equals("org.zonedabone.skygridmaker.SkyGen")) {
+			Block b = e.getBlock();
+			boolean cancel = true;
+			for (BlockFace bf : faces) {
+				if (b.getRelative(bf).getType() != Material.AIR) {
+					cancel = false;
+				}
+			}
+			if (cancel) {
+				e.setCancelled(true);
+			}
+		}
 	}
 }
